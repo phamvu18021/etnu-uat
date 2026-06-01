18e2bf29140644f55312690d247f954b7b8c97bd
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2026-06-01 09:56:04 +0700

    up

--- src/features/tuyen-sinh-tu-xa/components/CertificateBanner.tsx
+++ src/features/tuyen-sinh-tu-xa/components/CertificateBanner.tsx
@@ -1,11 +1,20 @@
 import { Box } from "@chakra-ui/react";
 import Image from "next/image";
 
-export const CertificateBanner = ({ CertificateBanner }: { CertificateBanner: any }) => {
+export const CertificateBanner = ({
+  CertificateBanner
+}: {
+  CertificateBanner: any;
+}) => {
   return (
-    <Box position="relative" w="100%" h={{ base: "300px", md: "500px", xl: "760px" }} overflow="hidden">
+    <Box
+      position="relative"
+      w="100%"
+      h={{ base: "300px", md: "500px", xl: "760px" }}
+      overflow="hidden"
+    >
       <Image
-        src={CertificateBanner?.banner_image || CertificateBanner?.url || CertificateBanner || "/banner.png"}
+        src={CertificateBanner?.banner_image || "/anh-bia-facebook.webp"}
         alt="Banner"
         fill
         style={{ objectFit: "cover", objectPosition: "center" }}
